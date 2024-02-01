427556cad56a0a3b171c6fb921e9c51305c04305
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-01 21:11:23 +0100

    test: Test CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-start p-24">
             <Image src={hopplaLogo} alt="Hoppla Logo" width={200} height={200}/>
-            Hoppla.ge - Coming Soon
+            Hoppla.ge - Coming Soon!
         </main>
     );
 }
