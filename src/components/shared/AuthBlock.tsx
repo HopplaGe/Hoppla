c2670cebc0fc6c48195b96dc5f3bbc9a327a86ba
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-03 18:26:00 +0100

    chore: add AuthJs and Prisma

---
+++ src/components/shared/AuthBlock.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {User} from "@nextui-org/user";
+import {Button} from "@nextui-org/react";
+import {signIn} from "next-auth/react";
+import AuthBtn from "@/components/shared/AuthBtn";
+import UserDropdownMenu from "@/components/shared/UserDropdownMenu";
+
+type AuthBlockProps = {
+    isLoggedIn: boolean;
+    user: any;
+    t: any;
+}
+
+const AuthBlock = ({isLoggedIn, user, t}: AuthBlockProps) => {
+
+    return (
+        <>
+            {isLoggedIn ? (
+                <UserDropdownMenu user={user} t={t}/>
+            ) : (
+                <AuthBtn t={t}/>
+            )}
+        </>
+    );
+};
+
+export default AuthBlock;
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,26 +1,30 @@
 "use client";
 import React from "react";
-import { NextUIProvider } from "@nextui-org/react";
-import { NextIntlClientProvider } from "next-intl";
+import {NextUIProvider} from "@nextui-org/react";
+import {NextIntlClientProvider} from "next-intl";
 import _ from "lodash";
+import {SessionProvider} from "next-auth/react";
 
 type ProvidersProps = {
-  children: React.ReactNode;
-  locale: string;
-  messages: any;
+    children: React.ReactNode;
+    locale: string;
+    messages: any;
 };
 
-const Providers = ({ children, locale, messages }: ProvidersProps) => {
-  return (
-    <NextIntlClientProvider
-      onError={() => {}}
-      //   getMessageFallback
-      locale={locale}
-      messages={messages}
-    >
-      <NextUIProvider>{children}</NextUIProvider>
-    </NextIntlClientProvider>
-  );
+const Providers = ({children, locale, messages}: ProvidersProps) => {
+    return (
+        <NextIntlClientProvider
+            onError={() => {
+            }}
+            //   getMessageFallback
+            locale={locale}
+            messages={messages}
+        >
+            <SessionProvider>
+                <NextUIProvider>{children}</NextUIProvider>
+            </SessionProvider>
+        </NextIntlClientProvider>
+    );
 };
 
 export default Providers;
