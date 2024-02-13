bcde8fe1fef81174011f8e53afbd70604b1d1d0d
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-13 23:47:32 +0100

    Add getUserById function

--- src/lib/actions/users.ts
+++ src/lib/actions/users.ts
@@ -1,3 +1,4 @@
+"use server"
 import prisma from "@/lib/prisma";
 
 
@@ -8,6 +9,20 @@ export const getUserByEmail = async (email: string) => {
         return await prisma.user.findUnique({
             where: {
                 email,
+            },
+        });
+    } catch (error) {
+        return null;
+    }
+}
+
+// find user by id
+
+export const getUserById = async (id: string) => {
+    try {
+        return await prisma.user.findUnique({
+            where: {
+                id,
             },
         });
     } catch (error) {
