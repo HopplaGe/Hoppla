eaa70a0d9c02797bf2af3fe69e26b4cb98515e4a
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-13 23:46:22 +0100

    Refactor RidesResultList component

--- src/components/rides/RidesResultList.tsx
+++ src/components/rides/RidesResultList.tsx
@@ -14,24 +14,32 @@ type RideListType = {
         transport_type?: string;
         sort?: string;
         filter?: string;
-    }
+    },
+    rides: any
 }
 
 
-const RidesResultList = ({rides}: { rides: any }) => {
+const RidesResultList = ({rides, searchParams}: RideListType) => {
 
-    console.log(rides);
-
-    const t = useTranslations("Rides");
+    const t = useTranslations("Rides.ResultList");
 
     return (
         <>
-            <h2 className="text-2xl font-bold">Results</h2>
+            <h2 className="text-2xl font-bold fira-go">{t("Results")}</h2>
+            <div className='mb-4 fira-go'>
+                <p className="text-gray-500 text-xs">
+                    {searchParams.from} - {searchParams.to}
+                </p>
+                <p className="text-gray-500 text-xs">
+                    {t("FoundRide", {count: rides.length})}
+                </p>
+            </div>
+
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
                 {rides.map((ride: Ride) => (
                     <div key={ride.id}
-                         className='bg-white shadow-lg border border-gray-200 rounded-xl  overflow-hidden'>
-                        <RideCard ride={ride} driver={null}/>
+                         className='bg-white shadow-md rounded-xl overflow-hidden'>
+                        <RideCard ride={ride} searchParams={searchParams}/>
                     </div>
                 ))}
             </div>
