7cb87668c36044718ff54966acdceb842a3a522b
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-13 23:45:47 +0100

    Refactor code formatting and add new components for filtering and sorting

--- src/app/[locale]/(public)/(ride)/search/page.tsx
+++ src/app/[locale]/(public)/(ride)/search/page.tsx
@@ -1,10 +1,10 @@
-import React, {Suspense} from 'react';
+import React, { Suspense } from 'react';
 import SearchBox from "@/components/partials/SearchBox";
 import RidesResultList from "@/components/rides/RidesResultList";
-import {getRideByFromAndToAndDateAndSeats} from '@/lib/actions/rides';
-import {Tabs, Tab} from "@nextui-org/react";
-import {useTranslations} from 'next-intl';
+import { getRideByFromAndToAndDateAndSeats } from '@/lib/actions/rides';
+import { RadioGroup, CheckboxGroup, Checkbox } from "@nextui-org/react";
 import CarTypesTab from "./_components/CarTypesTab";
+import { CustomRadio } from './_components/CustomRadio';
 
 interface pageProps {
     searchParams: {
@@ -18,45 +18,57 @@ interface pageProps {
     }
 }
 
-const Search = async ({searchParams}: pageProps) => {
-    const rides = await getRideByFromAndToAndDateAndSeats(searchParams.from, searchParams.to, searchParams.date, Number(searchParams.seats), searchParams.sort);
-    //
-    // console.log(rides);
+const Search = async ({ searchParams }: pageProps) => {
+    const rides = await getRideByFromAndToAndDateAndSeats(searchParams.from as string, searchParams.to, searchParams.date, Number(searchParams.seats), searchParams.sort);
 
     return (
         <>
             <div className="page-wrapper">
-                <SearchBox className="my-8"/>
+                <SearchBox className="my-8" />
                 <div className="pb-8 grid grid-cols-1 lg:grid-cols-4 gap-6">
                     <div className="">
-                        <div className="bg-white p-4 rounded-xl ">
-                            <h2 className="text-2xl font-bold">Filter</h2>
+                        <div className="bg-transparent rounded-xl fira-go">
+
+                            <RadioGroup label="ფილტრაცია">
+                                <CustomRadio value="free">
+                                    ფასის მიხედვით
+                                </CustomRadio>
+                                <CustomRadio value="pro">
+                                    დროს მიხედვით
+                                </CustomRadio>
+                                <CustomRadio
+                                    value="enterprise"
+                                >
+                                    ყველაზე ახლოს
+                                </CustomRadio>
+                            </RadioGroup>
                             <div className="mt-4">
-                                <h3 className="text-lg font-bold">Transport Type</h3>
-                                <div className="mt-2">
-                                    <input type="checkbox" id="car" name="car" value="car"/>
-                                    <label htmlFor="car"> Car</label><br/>
-                                    <input type="checkbox" id="bus" name="bus" value="bus"/>
-                                    <label htmlFor="bus"> Bus</label><br/>
-                                    <input type="checkbox" id="train" name="train" value="train"/>
-                                    <label htmlFor="train"> Train</label><br/>
-                                </div>
+                                <CheckboxGroup
+                                    label="ნდობა და უსაფრთხოება"
+                                    defaultValue={["buenos-aires"]}
+                                >
+                                    <Checkbox value="buenos-aires">ვერიფიცირებული მძღოლი</Checkbox>
+                                    <Checkbox value="london">რეიტინგული</Checkbox>
+                                </CheckboxGroup>
                             </div>
                             <div className="mt-4">
-                                <h3 className="text-lg font-bold">Sort</h3>
-                                <div className="mt-2">
-                                    <input type="radio" id="price" name="price" value="price"/>
-                                    <label htmlFor="price"> Price</label><br/>
-                                    <input type="radio" id="time" name="time" value="time"/>
-                                    <label htmlFor="time"> Time</label><br/>
-                                </div>
+                                <CheckboxGroup
+                                    label="წესები"
+                                    defaultValue={["buenos-aires"]}
+                                >
+                                    <Checkbox value="buenos-aires">სიგარეტის მოწევა</Checkbox>
+                                    <Checkbox value="london">ცხოველების ტრანსპორტირება</Checkbox>
+                                    <Checkbox value="london">კონდიციონერი</Checkbox>
+                                    <Checkbox value="london">ბარგი</Checkbox>
+
+                                </CheckboxGroup>
                             </div>
                         </div>
                     </div>
                     <div className="lg:col-span-3">
-                        <CarTypesTab transport_type={searchParams.transport_type as string}/>
+                        <CarTypesTab transport_type={searchParams.transport_type as string} />
                         <Suspense fallback={<div>Loading...</div>}>
-                            <RidesResultList rides={rides}/>
+                            <RidesResultList rides={rides} searchParams={searchParams} />
                         </Suspense>
                     </div>
                 </div>
