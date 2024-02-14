2ada72e865473d18a52d87a909d11dba3b335778
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-14 13:55:25 +0100

    Refactor Hero component and update button styles

--- src/components/partials/Hero.tsx
+++ src/components/partials/Hero.tsx
@@ -7,16 +7,17 @@ import SearchBox from "@/components/partials/SearchBox";
 import OfferingsSection from "@/app/[locale]/(public)/(ride)/_components/OfferingsSection";
 import Link from "next/link";
 import { LuLogIn } from "react-icons/lu";
-import { Button } from "../ui/button";
 import { MdOutlineTravelExplore } from "react-icons/md";
 import { Suspense } from "react";
+import { motion } from "framer-motion";
+import { Button } from "@nextui-org/react";
 
 const Hero = () => {
   const t = useTranslations("Hero");
 
   return (
     <>
-      <div className="page-wrapper grid lg:grid-cols-2 items-center pt-16 pb-8 md:pt-12 md:pb-24 fira-go">
+      <div className="page-wrapper grid lg:grid-cols-2 items-center py-16 pb-8 md:pt-12 fira-go">
         <Image
           src="/buses/booking.svg"
           alt="Main Image"
@@ -25,32 +26,41 @@ const Hero = () => {
           className="max-h-[400px] mb-10 md:mb-0 w-full block lg:order-last"
         />
         <div>
-          <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold lg:tracking-tight xl:tracking-tighter">
+          <motion.h1
+            className="text-4xl lg:text-5xl xl:text-6xl font-bold lg:tracking-tight xl:tracking-tighter"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+          >
             {t(`title`, { company: "HOPPLA" })}
-          </h1>
-          <p className="text-lg mt-4 text-gray-800 dark:text-gray-300 max-w-xl">
+          </motion.h1>
+          <motion.p
+            className="text-lg mt-4 text-gray-800 dark:text-gray-300 max-w-xl"
+            initial={{ opacity: 0, x: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+          >
             {t(`subtitle`)}
-          </p>
+          </motion.p>
           <div className="mt-6 flex flex-col sm:flex-row gap-3">
             <Button
               size="lg"
-              variant="default"
+              variant="solid"
+              color="primary"
               className="flex items-center gap-2"
-              asChild
+              startContent={<MdOutlineTravelExplore />}
             >
               <Link href="/carpool">
-                <MdOutlineTravelExplore /> <span>{t(`travelBtn`)}</span>
+                <span>{t(`travelBtn`)}</span>
               </Link>
             </Button>
 
             <Button
               size="lg"
-              variant="outline"
+              variant="light"
+              color="default"
               className="flex items-center gap-2"
-              asChild
+              startContent={<LuLogIn />}
             >
               <Link href="/signin">
-                <LuLogIn />
                 <span>{t(`loginBtn`)}</span>
               </Link>
             </Button>
@@ -58,11 +68,10 @@ const Hero = () => {
         </div>
       </div>
       <Suspense fallback={<div>Loading...</div>}>
-        <div className="page-wrapper"><SearchBox /></div>
+        <div className="page-wrapper py-12"><SearchBox /></div>
       </Suspense>
-      <div className="mb-10"></div>
-      <div className="page-wrapper py-8 fira-go">
-        <div className="mt-16 md:mt-0">
+      <div className="bg-default-100 py-28 fira-go">
+        <div className="page-wrapper mt-16 md:mt-0">
           <h2 className="text-4xl text-secondary lg:text-5xl font-bold lg:tracking-tight">
             {t(`aboutTitle`, { company: "HOPPLA" })}
           </h2>
@@ -72,9 +81,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="mb-10"></div>
       <OfferingsSection />
-      <div className="mb-20"></div>
     </>
   );
   // return (
