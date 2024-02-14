5348b325da4b65ad262baf5c08254bcded7eb76c
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-14 13:55:11 +0100

    Add MobileAppBlock component to Index page

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,5 +1,6 @@
 import { useTranslations } from "next-intl";
 import Hero from "@/components/partials/Hero";
+import phoneFrame from '@/assets/images/phone-frame.svg'
 import OfferingsSection from "./(ride)/_components/OfferingsSection";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
@@ -9,12 +10,14 @@ import { MdOutlineTravelExplore } from "react-icons/md";
 import { IoAddOutline } from "react-icons/io5";
 import { FiMapPin } from "react-icons/fi";
 import { LuLogIn } from "react-icons/lu";
+import MobileAppBlock from "@/components/partials/MobileAppBlock";
 
 export default function Index() {
-  // const t = useTranslations("Index");
+  const t = useTranslations("MobileAppBlock");
   return (
     <>
       <Hero />
+      <MobileAppBlock image={phoneFrame} title={t(`title`)} subtitle={t(`subtitle`)} />
     </>
   );
 }
