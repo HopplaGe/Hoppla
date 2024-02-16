dceeafb31d4f6fd7271a993edecc1641fab6737f
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-16 18:53:14 +0100

    Add stats and compare functions to stats.ts

--- src/lib/actions/stats.ts
+++ src/lib/actions/stats.ts
@@ -1,6 +1,11 @@
 "use server";
+import prisma from "@/lib/prisma";
+import moment from "moment";
+import "moment/locale/ka";
 
 import { getRides, getRidesByTo } from "./rides"
+import { compareLastMonthRides } from "../tools/compareLastMonthRides";
+import { compareLastMonthEarnings } from "../tools/compareLastMonthEarnings";
 
 export const allRidesCount = async () => {
     const rides = await getRides();
@@ -10,4 +15,63 @@ export const allRidesCount = async () => {
 export const ridesCountByDirection = async (to: string) => {
     const rides = await getRidesByTo(to);
     return rides?.length;
+}
+
+export async function getStats(userId: string) {
+
+    const cars = await prisma.car.count({
+        where: {
+            ownerId: userId
+        }
+    });
+
+    const rides = await prisma.ride.findMany({
+        where: {
+            driverId: userId,
+            status: "COMPLETED"
+        },
+        select: {
+            createdAt: true,
+        }
+    });
+
+    const earnings = await prisma.ride.findMany({
+        where: {
+            driverId: userId,
+            status: "COMPLETED"
+        },
+        select: {
+            price: true,
+            status: true,
+            createdAt: true,
+        }
+    });
+
+    //TODO: Rating and earnings statistics are to be added
+    return [
+        {
+            title: "მგზავრობა",
+            value: rides.length,
+            compare: {
+                percent: compareLastMonthRides(rides).status === "increase" ? `+${compareLastMonthRides(rides).percent}` : `-${compareLastMonthRides(rides).percent}` ? `0%` : `0%`,
+                status: compareLastMonthRides(rides).status
+            }
+        },
+        {
+            title: "შემოსავალი",
+            value: `${earnings.reduce((a, b) => a + b.price, 0).toFixed(2)} ₾`,
+            compare: {
+                percent: compareLastMonthEarnings(earnings).status === "increase" ? `+${compareLastMonthEarnings(earnings).percent}` : `-${compareLastMonthEarnings(earnings).percent}` ? parseFloat(compareLastMonthEarnings(earnings).percent) > 0 ? `-${compareLastMonthEarnings(earnings).percent}` : `0%` : `0%`,
+                status: compareLastMonthEarnings(earnings).status
+            }
+        },
+        // {
+        //     title: "ავტომობილები",
+        //     value: cars
+        // },
+        {
+            title: "რეიტინგი",
+            value: 4.5
+        }
+    ];
 }
