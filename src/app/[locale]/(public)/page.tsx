fe4cd83b5022fc80eda7b5efa698fae73090cfe4
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-16 16:29:09 +0100

    Refactor page component and add search box and map

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,21 +1,33 @@
 import { useTranslations } from "next-intl";
 import Hero from "@/components/partials/Hero";
 import OfferingsSection from "./(ride)/_components/OfferingsSection";
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import { HiOutlineSparkles } from "react-icons/hi2";
-import { MdOutlineTravelExplore } from "react-icons/md";
-import { IoAddOutline } from "react-icons/io5";
-import { FiMapPin } from "react-icons/fi";
-import { LuLogIn } from "react-icons/lu";
 import MobileAppBlock from "@/components/partials/MobileAppBlock";
+import { Suspense } from "react";
+import SearchBox from "@/components/partials/SearchBox";
+import SvgMap from "@/components/shared/maps/SvgMap";
+import { regions } from "@/lib/data/regions";
 
 export default function Index() {
- 
+  const t = useTranslations("Hero");
   return (
     <>
       <Hero />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="page-wrapper py-12"><SearchBox /></div>
+      </Suspense>
+      <div className="bg-default-100 py-28 fira-go">
+        <div className="page-wrapper mt-16 md:mt-0">
+          <h2 className="text-4xl text-secondary lg:text-5xl font-bold lg:tracking-tight">
+            {t(`aboutTitle`, { company: "HOPPLA" })}
+          </h2>
+          <p className="text-lg mt-4 text-gray-600 dark:text-gray-300">
+            {t(`aboutDesc`)}
+          </p>
+        </div>
+        <SvgMap regions={regions}/>
+      </div>
+
+      <OfferingsSection />
       <MobileAppBlock />
     </>
   );
