4fe71b917cc336bb30748996da69545ed0ed33a5
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-19 22:40:50 +0100

    Refactor RideDetailHead component

--- src/app/[locale]/(public)/(ride)/ride/_components/RideDetailHead.tsx
+++ src/app/[locale]/(public)/(ride)/ride/_components/RideDetailHead.tsx
@@ -9,17 +9,17 @@ import React from 'react'
 const RideDetailHead = (startDate: any) => {
   const locale = useLocale()
   return (
-    <div className="flex flex-row mx-auto w-1/2 max-w-7xl relative justify-center items-center">
+    <div className="flex flex-row mx-auto w-full lg:w-1/2 max-w-7xl relative justify-center items-center -translate-x-8">
           <Button
             size="sm"
             variant="back"
             onClick={() => window.history.back()}
             // startContent={<ChevronLeft size={44}/>}
-            className='absolute left-10 top-1'
+        // className='absolute left-10 top-1'
           >
-            <ChevronLeft size={44}/>
+        <ChevronLeft className='w-8 h-8 md:w-10 md:h-10' />
           </Button>
-          <h1 className='text-4xl text-center fira-go'>
+      <h1 className='text-2xl md:text-4xl text-center fira-go'>
             {moment(startDate).locale(locale).format('LL').slice(0, -5)}
           </h1>
         </div>
