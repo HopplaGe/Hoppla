a853298beda2ee51d59e5240ad9eda468397bb96
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-19 22:42:30 +0100

    Refactor RidesResultList component

--- src/components/rides/RidesResultList.tsx
+++ src/components/rides/RidesResultList.tsx
@@ -24,7 +24,7 @@ const RidesResultList = ({rides, searchParams}: RideListType) => {
     const t = useTranslations("Rides.ResultList");
 
     return (
-        <>
+        <div className='flex flex-col gap-4'>
             <h2 className="text-2xl font-bold fira-go">{t("Results")}</h2>
             <div className='mb-4 fira-go'>
                 <p className="text-gray-500 text-xs">
@@ -43,7 +43,7 @@ const RidesResultList = ({rides, searchParams}: RideListType) => {
                     </div>
                 ))}
             </div>
-        </>
+        </div>
     );
 };
 
