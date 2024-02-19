d0984ac131a958f91c3728aca27b05e48b694f24
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-19 22:38:46 +0100

    Update grid layout in Regions component

--- src/app/[locale]/(administration)/manage/populated-areas/_components/Regions.tsx
+++ src/app/[locale]/(administration)/manage/populated-areas/_components/Regions.tsx
@@ -15,7 +15,7 @@ const Regions = ({ regions }: RegionsProps) => {
         <div className='flex flex-col gap-2'>
             <h2 className='text-xl'>რეგიონები</h2>
 
-            <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-2'>
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-2'>
                 {regions?.map((region: Region, index: number) => (
                     <RegionCard key={index} region={region} />
                 ))}
