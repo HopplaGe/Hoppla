7c445358eaa317289db0cf6b4b5d87646dce58d5
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-20 16:36:52 +0100

    Refactor code formatting and indentation

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,37 +1,37 @@
-import { useLocale } from "next-intl";
+import {useLocale} from "next-intl";
 import Hero from "@/components/partials/Hero";
 import OfferingsSection from "./(ride)/_components/OfferingsSection";
 import MobileAppBlock from "@/components/partials/MobileAppBlock";
-import { Suspense } from "react";
+import {Suspense} from "react";
 import SearchBox from "@/components/partials/SearchBox";
 import SvgMap from "@/components/shared/maps/SvgMap";
-import { getCountry } from "@/lib/actions/countries";
+import {getCountry} from "@/lib/actions/countries";
 import About from "@/components/shared/About";
 
 const Home = async () => {
-  const local = useLocale();
+    const local = useLocale();
 
-  const countryId = local === "ka" ? "clstny1yy00008ku7qe62hb1z" : "clsreu24j0001j6z8pv4sb5vd";
+    const countryId = local === "ka" ? "clsrda0sc0000j6z873ebw7wv" : "clsreu24j0001j6z8pv4sb5vd";
 
-  const country = await getCountry(countryId);
+    const country = await getCountry(countryId);
 
-  return (
-    <>
-      <Hero />
-      <Suspense fallback={<div>Loading...</div>}>
-        <div className="page-wrapper py-12"><SearchBox /></div>
-      </Suspense>
-      <div className="bg-default-100 py-28 fira-go">
-        <div className="page-wrapper mt-16 md:mt-0">
-          <About/>
-        </div>
-        <SvgMap regions={country?.regions}/>
-      </div>
+    return (
+        <>
+            <Hero/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <div className="page-wrapper py-12"><SearchBox/></div>
+            </Suspense>
+            <div className="bg-default-100 py-4 md:py-14 lg:py-28 fira-go">
+                <div className="page-wrapper mt-6 md:mt-0 mb-8 lg:mb-0">
+                    <About/>
+                </div>
+                <SvgMap regions={country?.regions}/>
+            </div>
 
-      <OfferingsSection />
-      <MobileAppBlock />
-    </>
-  );
+            <OfferingsSection/>
+            <MobileAppBlock/>
+        </>
+    );
 }
 
 export default Home;
