4fbfab4c1e56c8d6d2a34dd503579ab69f7578cb
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-20 16:37:16 +0100

    Refactor About component's styling and imports

--- src/components/shared/About.tsx
+++ src/components/shared/About.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useTranslations } from 'next-intl';
+import {useTranslations} from 'next-intl';
 import React from 'react'
 
 const About = () => {
@@ -8,10 +8,10 @@ const About = () => {
 
     return (
         <>
-            <h2 className="text-4xl text-secondary lg:text-5xl font-bold lg:tracking-tight">
-                {t(`aboutTitle`, { company: "HOPPLA" })}
+            <h2 className="text-xl md:text-4xl text-secondary lg:text-5xl font-bold lg:tracking-tight">
+                {t(`aboutTitle`, {company: "HOPPLA"})}
             </h2>
-            <p className="text-lg mt-4 text-gray-600 dark:text-gray-300">
+            <p className="text-xs md:text-md lg:text-lg mt-4 text-gray-600 dark:text-gray-300">
                 {t(`aboutDesc`)}
             </p>
         </>
