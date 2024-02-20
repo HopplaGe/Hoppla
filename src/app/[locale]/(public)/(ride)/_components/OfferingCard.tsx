8eab8ab787396815641d7dd315c267746453bb6e
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-20 16:36:56 +0100

    Refactor OfferingCard component

--- src/app/[locale]/(public)/(ride)/_components/OfferingCard.tsx
+++ src/app/[locale]/(public)/(ride)/_components/OfferingCard.tsx
@@ -1,24 +1,24 @@
 import {ElementType} from "react";
 
 type OfferingCardProps = {
-  title: string;
-  description: string;
-  icon: ElementType;
+    title: string;
+    description: string;
+    icon: ElementType;
 };
 
 export default function OfferingCard({
-  description,
-  icon,
-  title,
-}: OfferingCardProps) {
-  const Icon = icon;
-  return (
-    <li className="flex flex-col gap-1 hover:bg-default-100 py-16 px-4 hoppla-animation hover:rounded-t-xl">
-      <div className="mb-1">
-        <Icon className="text-black text-5xl" />
-      </div>
-      <h3 className="text-lg font-semibold">{title}</h3>
-      <p className="text-gray-600">{description}</p>
-    </li>
-  );
+                                         description,
+                                         icon,
+                                         title,
+                                     }: OfferingCardProps) {
+    const Icon = icon;
+    return (
+        <li className="flex flex-col gap-1 hover:bg-default-100 py-4 md:py-8 lg:py-16 px-4 hoppla-animation hover:rounded-xl">
+            <div className="mb-1">
+                <Icon className="text-black text-2xl md:text-4xl lg:text-5xl"/>
+            </div>
+            <h3 className="text-sm md:text-md lg:text-lg font-semibold">{title}</h3>
+            <p className="text-xs md:text-sm text-gray-600">{description}</p>
+        </li>
+    );
 }
