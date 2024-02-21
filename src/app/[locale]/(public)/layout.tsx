4167fe50e92343d519820876f3ee8f9aeae1c02e
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-21 14:02:33 +0100

    Refactor PublicLayout component to include TopInfoBanner

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -1,16 +1,23 @@
 import React from "react";
-import { LayoutProps } from "@/types/layoutProps";
+import {LayoutProps} from "@/types/layoutProps";
 import NavBar from "@/components/partials/NavBar";
 import Footer from "@/components/partials/Footer";
+import TopInfoBanner from "@/components/shared/TopInfoBanner";
+import {getUrgentInfo} from "@/lib/actions/infos";
+import {UrgentInfo} from "@prisma/client";
 
-const PublicLayout = ({ children }: LayoutProps) => {
-  return (
-    <>
-      <NavBar />
-      {children}
-      <Footer />
-    </>
-  );
+const PublicLayout = async ({children}: LayoutProps) => {
+
+    const info = await getUrgentInfo();
+
+    return (
+        <>
+            <NavBar/>
+            {info && <TopInfoBanner info={info as UrgentInfo}/>}
+            {children}
+            <Footer/>
+        </>
+    );
 };
 
 export default PublicLayout;
