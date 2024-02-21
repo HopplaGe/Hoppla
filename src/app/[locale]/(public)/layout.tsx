7486a188546076195010f6f87e3fd0bdf9468d83
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-21 21:32:54 +0100

    Fix formatting and comment out unused code in layout.tsx

--- src/app/[locale]/(public)/layout.tsx
+++ src/app/[locale]/(public)/layout.tsx
@@ -3,19 +3,19 @@ import {LayoutProps} from "@/types/layoutProps";
 import NavBar from "@/components/partials/NavBar";
 import Footer from "@/components/partials/Footer";
 import TopInfoBanner from "@/components/shared/TopInfoBanner";
-import { getUrgentInfo } from "@/lib/actions/infos";
+import {getUrgentInfo} from "@/lib/actions/infos";
 
 const PublicLayout = async ({children}: LayoutProps) => {
 
     // TODO: Fix this "წარმოებაში გამოაქვს შეცდომა"
     // const info = await getUrgentInfo();
-
+    //
     // console.log(info);
 
     return (
         <>
             <NavBar/>
-            {/* {info && <TopInfoBanner info={info[0]} />} */}
+            {/*{info && <TopInfoBanner info={info[0]}/>}*/}
             {children}
             <Footer/>
         </>
