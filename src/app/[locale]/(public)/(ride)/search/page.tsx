9246cf7dad8e038009acda08cd8af1af43bfce5e
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-23 03:48:24 +0100

    Refactor search page component and add filter component

--- src/app/[locale]/(public)/(ride)/search/page.tsx
+++ src/app/[locale]/(public)/(ride)/search/page.tsx
@@ -1,10 +1,14 @@
-import React, { Suspense } from 'react';
+import React, {Suspense} from 'react';
 import SearchBox from "@/components/partials/SearchBox";
 import RidesResultList from "@/components/rides/RidesResultList";
-import { getRideByFromAndToAndDateAndSeats } from '@/lib/actions/rides';
-import { RadioGroup, CheckboxGroup, Checkbox } from "@nextui-org/react";
+import {getRideByFromAndToAndDateAndSeats} from '@/lib/actions/rides';
 import CarTypesTab from "./_components/CarTypesTab";
-import { CustomRadio } from './_components/CustomRadio';
+import Filter from "./_components/Filter";
+import {
+    QueryClient,
+    HydrationBoundary,
+    dehydrate
+} from "@tanstack/react-query";
 
 interface pageProps {
     searchParams: {
@@ -18,62 +22,29 @@ interface pageProps {
     }
 }
 
-const Search = async ({ searchParams }: pageProps) => {
-    const rides = await getRideByFromAndToAndDateAndSeats(searchParams.from as string, searchParams.to, searchParams.date, Number(searchParams.seats), searchParams.sort);
+const Search = async ({searchParams}: pageProps) => {
+    const queryClient = new QueryClient();
+
+    await queryClient.prefetchQuery({
+        queryKey: ['rides', searchParams],
+        queryFn: () => getRideByFromAndToAndDateAndSeats(searchParams),
+    });
 
     return (
-        <>
+        <HydrationBoundary state={dehydrate(queryClient)}>
             <div className="page-wrapper">
-                <SearchBox className="my-8" />
+                <SearchBox className="my-8"/>
                 <div className="pb-8 grid grid-cols-1 lg:grid-cols-4 gap-6">
-                    <div className="hidden lg:block">
-                        <div className="bg-transparent rounded-xl fira-go">
-
-                            <RadioGroup label="ფილტრაცია">
-                                <CustomRadio value="free">
-                                    ფასის მიხედვით
-                                </CustomRadio>
-                                <CustomRadio value="pro">
-                                    დროს მიხედვით
-                                </CustomRadio>
-                                <CustomRadio
-                                    value="enterprise"
-                                >
-                                    ყველაზე ახლოს
-                                </CustomRadio>
-                            </RadioGroup>
-                            <div className="mt-4">
-                                <CheckboxGroup
-                                    label="ნდობა და უსაფრთხოება"
-                                    defaultValue={["buenos-aires"]}
-                                >
-                                    <Checkbox value="buenos-aires">ვერიფიცირებული მძღოლი</Checkbox>
-                                    <Checkbox value="london">რეიტინგული</Checkbox>
-                                </CheckboxGroup>
-                            </div>
-                            <div className="mt-4">
-                                <CheckboxGroup
-                                    label="წესები"
-                                    defaultValue={["buenos-aires"]}
-                                >
-                                    <Checkbox value="buenos-aires">სიგარეტის მოწევა</Checkbox>
-                                    <Checkbox value="london">ცხოველების ტრანსპორტირება</Checkbox>
-                                    <Checkbox value="london">კონდიციონერი</Checkbox>
-                                    <Checkbox value="london">ბარგი</Checkbox>
-
-                                </CheckboxGroup>
-                            </div>
-                        </div>
-                    </div>
+                    <Filter/>
                     <div className="lg:col-span-3 flex flex-col gap-4">
-                        <CarTypesTab transport_type={searchParams.transport_type as string} />
+                        <CarTypesTab transport_type={searchParams.transport_type as string}/>
                         <Suspense fallback={<div>Loading...</div>}>
-                            <RidesResultList rides={rides} searchParams={searchParams} />
+                            <RidesResultList searchParams={searchParams}/>
                         </Suspense>
                     </div>
                 </div>
             </div>
-        </>
+        </HydrationBoundary>
     );
 };
 
