0bb5721d9533ecb23b03a50f85feb82d495f0e6b
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-23 15:39:41 +0000

    Refactor Filter component and getAllRules function

--- src/app/[locale]/(public)/(ride)/search/_components/Filter.tsx
+++ src/app/[locale]/(public)/(ride)/search/_components/Filter.tsx
@@ -11,52 +11,60 @@ const Filter = () => {
 
     const {data, isLoading, error} = useRules();
 
-    if (isLoading) return <div>Loading...</div>
-    if (error) return <div>{error.message}</div>
+    if(data) console.log(data);
 
     return (
-        <div className="hidden lg:block">
-            <div className="bg-transparent rounded-xl fira-go">
+        <>
+        filter
+        </>
+    )
 
-                <RadioGroup
-                    label="ფილტრაცია"
-                    onValueChange={setSort}
-                    defaultChecked
-                    defaultValue={sort}
-                >
-                    <CustomRadio value="price-asc">
-                        ფასის მიხედვით
-                    </CustomRadio>
-                    <CustomRadio value="time-asc">
-                        დროს მიხედვით
-                    </CustomRadio>
-                </RadioGroup>
-                <div className="mt-4">
-                    <CheckboxGroup
-                        label="ნდობა და უსაფრთხოება"
-                        defaultValue={[filter as string]}
-                        onValueChange={setFilter as any}
-                    >
-                        <Checkbox value="verified">ვერიფიცირებული მძღოლი</Checkbox>
-                        <Checkbox value="rating">რეიტინგული</Checkbox>
-                    </CheckboxGroup>
-                </div>
-                <div className="mt-4">
-                    <CheckboxGroup
-                        label="წესები"
-                        defaultValue={[rules as string]}
-                        onValueChange={setRules as any}
-                    >
-                        {Array.isArray(data) && data.map((rule: any) => (
-                            <Checkbox key={rule.id} value={rule.id}>
-                                {rule.name}
-                            </Checkbox>
-                        ))}
-                    </CheckboxGroup>
-                </div>
-            </div>
-        </div>
-    );
+    // if (isLoading) return <div>Loading...</div>
+    // if (error) return <div>{error.message}</div>
+
+    // return (
+    //     <div className="hidden lg:block">
+    //         <div className="bg-transparent rounded-xl fira-go">
+
+    //             <RadioGroup
+    //                 label="ფილტრაცია"
+    //                 onValueChange={setSort}
+    //                 defaultChecked
+    //                 defaultValue={sort}
+    //             >
+    //                 <CustomRadio value="price-asc">
+    //                     ფასის მიხედვით
+    //                 </CustomRadio>
+    //                 <CustomRadio value="time-asc">
+    //                     დროს მიხედვით
+    //                 </CustomRadio>
+    //             </RadioGroup>
+    //             <div className="mt-4">
+    //                 <CheckboxGroup
+    //                     label="ნდობა და უსაფრთხოება"
+    //                     defaultValue={[filter as string]}
+    //                     onValueChange={setFilter as any}
+    //                 >
+    //                     <Checkbox value="verified">ვერიფიცირებული მძღოლი</Checkbox>
+    //                     <Checkbox value="rating">რეიტინგული</Checkbox>
+    //                 </CheckboxGroup>
+    //             </div>
+    //             <div className="mt-4">
+    //                 <CheckboxGroup
+    //                     label="წესები"
+    //                     defaultValue={[rules as string]}
+    //                     onValueChange={setRules as any}
+    //                 >
+    //                     {Array.isArray(data) && data.map((rule: any) => (
+    //                         <Checkbox key={rule.id} value={rule.id}>
+    //                             {rule.name}
+    //                         </Checkbox>
+    //                     ))}
+    //                 </CheckboxGroup>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
 };
 
 export default Filter;
