83be7d14590dec253ffeddd768e8e13d699b742f
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-23 03:47:52 +0100

    Refactor RatingsBlock component to accept ratings prop

--- src/app/[locale]/(public)/(users)/user/[userId]/_components/RatingsBlock.tsx
+++ src/app/[locale]/(public)/(users)/user/[userId]/_components/RatingsBlock.tsx
@@ -3,8 +3,17 @@ import React from 'react';
 import {Star} from "lucide-react";
 import {useTranslations} from "next-intl";
 
-const RatingsBlock = () => {
+type RatingsBlockType = {
+    ratings: any
+}
+
+const RatingsBlock = ({
+                          ratings
+                      }: RatingsBlockType) => {
     const t = useTranslations("UserInformation");
+
+    // console.log(ratings)
+
     return (
         <div
             className='relative group rounded-xl overflow-hidden border-y-0 p-4 fira-go border-default-100 w-full flex flex-row justify-between items-center gap-4'>
@@ -13,13 +22,13 @@ const RatingsBlock = () => {
                     <Star size={16}/>
                 </i>
                 <span className="text-medium lg:text-lg font-bold">
-                        4.3 / 5
+                    {ratings?.length > 0 ? (ratings?.reduce((acc: number, rating: any) => acc + rating.rating, 0) / ratings?.length).toFixed(1) : 0} / 5
                     </span>
             </div>
             <div className="flex flex-row justify-start items-center gap-2">
                 <span className="text-xs md:text-sm">
                     {t.rich('Ratings', {
-                            rating: 0,
+                            rating: ratings?.length,
                             ratingCount: (chunks) => <span
                                 className="text-xs md:text-sm font-bold text-primary">{chunks}</span>
                         }
