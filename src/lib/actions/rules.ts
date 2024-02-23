fa147b32ce7108cb187817ecf19c8c568534f541
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-23 03:45:06 +0100

    Add getAllRules function to rules.ts

---
+++ src/lib/actions/rules.ts
@@ -0,0 +1,12 @@
+"use server"
+import prisma from "@/lib/prisma";
+
+export async function getAllRules() {
+    try {
+        return await prisma.rule.findMany(
+
+        );
+    } catch (error) {
+        return {error};
+    }
+}
