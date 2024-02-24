0d9918994e5f87a7588c118a728e000641feba9e
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-24 04:11:46 +0100

    Add authentication and display rates for a specific user

--- src/app/[locale]/(private)/dashboard/(with-stats)/rates/page.tsx
+++ src/app/[locale]/(private)/dashboard/(with-stats)/rates/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import {auth} from "@/lib/auth";
+import RatesHeader from "@/app/[locale]/(private)/dashboard/(with-stats)/rates/_components/RatesHeader";
+import RatesList from "@/app/[locale]/(private)/dashboard/(with-stats)/rates/_components/RatesList";
 
-const DashRides = () => {
+const DashRides = async () => {
+    const session = await auth();
+    const user = session?.user;
+
     return (
-        <div>
-            Rates
+        <div className="w-full flex flex-col justify-start items-center gap-4">
+            <RatesHeader userId={user?.id!}/>
+            <RatesList userId={user?.id!}/>
         </div>
     );
 };
