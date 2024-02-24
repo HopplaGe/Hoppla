2efa8c08d2af3f0bd89129cc6285e093582700f4
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-24 04:14:37 +0100

    Add user-ratings action file

---
+++ src/lib/actions/ratings/user-ratings.ts
@@ -0,0 +1,24 @@
+"use server"
+import prisma from "@/lib/prisma";
+
+export const getUserRatings = async (userId: string) => {
+    const ratings = await prisma.userRating.findMany({
+        where: {
+            userId: userId
+        },
+        select: {
+            rating: true,
+            comment: true,
+            createdAt: true,
+            author: true
+        }
+    });
+
+    const ratingAvg = ratings.reduce((a, b) => a + b.rating, 0) / ratings.length;
+
+    return {
+        ratingAvg: ratingAvg.toFixed(1),
+        ratingCount: ratings.length,
+        ratings: ratings
+    };
+}
