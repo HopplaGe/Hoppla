87765d1f30c763e6c24f06624cc1883af03c7212
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-24 04:13:06 +0100

    Refactor StatBlock component to use useUserStats hook

--- src/components/dashboard/stats/StatBlock.tsx
+++ src/components/dashboard/stats/StatBlock.tsx
@@ -1,25 +1,31 @@
-import React, { FC } from 'react'
+"use client"
+import React, {FC} from 'react'
 import StatCard from './StatCard'
-import { User } from '@prisma/client'
-import { getStats } from '@/lib/actions/stats'
-import { BarChart2} from 'lucide-react'
+import {User} from '@prisma/client'
+import {BarChart2} from 'lucide-react'
+import {useUserStats} from "@/hooks/statistics/useUserStats";
 
 type StatBlockProps = {
     user: User
 }
 
-const StatBlock: FC<StatBlockProps> = async ({ user }) => {
-    const stats = await getStats(user?.id!);
+const StatBlock: FC<StatBlockProps> = ({user}) => {
+
+    const {data: stats, isLoading, error} = useUserStats(user)
+
+    if (isLoading) return <div>Loading...</div>
+    if (error) return <div>Error: {error.message}</div>
+
     return (
         <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2'>
-            {stats.map((stat, index) => (
+            {stats?.map((stat, index) => (
                 <StatCard
                     key={index}
                     title={stat.title}
                     value={stat.value}
                     data={null}
                     color='light'
-                    icon={<BarChart2 size={28} />}
+                    icon={<BarChart2 size={28}/>}
                     percent={stat?.compare?.percent}
                     status={stat?.compare?.status}
                 />
