627d786bf1984045cab0427e7cc0c9344feef73a
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-02-24 04:14:06 +0100

    Update useRides.ts to include useDailyRides hook

--- src/hooks/rides/useRides.ts
+++ src/hooks/rides/useRides.ts
@@ -1,10 +1,17 @@
 import {useQuery} from "@tanstack/react-query";
-import {getAreasByRegionId} from "@/lib/actions/populated-areas";
-import {getRideByFromAndToAndDateAndSeats} from "@/lib/actions/rides";
+import {getRideByFromAndToAndDateAndSeats, getRidesByDate} from "@/lib/actions/rides/get";
+import moment from "moment/moment";
 
 export const useRides = (searchParams: any) => {
     return useQuery({
         queryKey: ['rides', searchParams],
         queryFn: async () => getRideByFromAndToAndDateAndSeats(searchParams)
+    })
+}
+
+export const useDailyRides = () => {
+    return useQuery({
+        queryKey: ['dailyRides'],
+        queryFn: async () => getRidesByDate(new Date(moment().utc().startOf('day').format('YYYY-MM-DDTHH:mm:ss')))
     })
 }
