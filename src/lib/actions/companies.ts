73ef3f749907c6815aa81bed2e30343c0cc9ac66
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-02 02:01:45 +0100

    Refactor getCompanies and createCompany functions

--- src/lib/actions/companies.ts
+++ src/lib/actions/companies.ts
@@ -2,19 +2,28 @@
 import prisma from "@/lib/prisma";
 
 export const getCompanies = async () => {
-    try {
-        return await prisma.rule.findMany();
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    return await prisma.company.findMany({
+      orderBy: {
+        createdAt: "asc",
+      },
+      include: {
+        owner: true,
+        cars: true,
+        drivers: true,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export const createCompany = async (data: any) => {
-    try {
-        return await prisma.rule.create({
-            data
-        });
-    } catch (error) {
-        console.error(error)
-    }
-}
+  try {
+    return await prisma.rule.create({
+      data,
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
