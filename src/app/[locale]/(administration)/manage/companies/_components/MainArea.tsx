af48b532115adfe9adaba41a96f8c26c9226a2d5
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-02 01:58:28 +0100

    Refactor MainArea component to include company search and table

--- src/app/[locale]/(administration)/manage/companies/_components/MainArea.tsx
+++ src/app/[locale]/(administration)/manage/companies/_components/MainArea.tsx
@@ -1,9 +1,40 @@
-import React, {FC} from "react";
+"use client";
+import React, { FC } from "react";
+import Table from "./Table";
+import TableRow from "./TableRow";
+import CompanySearch from "./CompanySearch";
+import { useCompanies } from "@/hooks/companies/useCompanies";
+import { Company } from "@prisma/client";
 
-const MainArea: FC = () => (
-    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-        Main area
+const MainArea: FC = () => {
+  const [companySearchQuery, setCompanySearchQuery] = React.useState("");
+
+  const { data: companies, isLoading, isError } = useCompanies();
+
+  const filteredCompanies = companies?.filter((company) => {
+    return company.name
+      .toLowerCase()
+      .includes(companySearchQuery.toLowerCase());
+  }) as Company[];
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Error</div>;
+  }
+
+  return (
+    <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6 space-y-6">
+      <CompanySearch setCompanySearchQuery={setCompanySearchQuery} />
+      <Table>
+        {filteredCompanies?.map((company) => (
+          <TableRow key={company.id} item={company} />
+        ))}
+      </Table>
     </div>
-)
+  );
+};
 
-export default MainArea
+export default MainArea;
