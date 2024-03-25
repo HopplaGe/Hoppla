99b8da6f4a48bd2444828678e8f6a5472624a671
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-25 21:22:13 +0100

    Refactor TopInfoBanner component

--- src/components/shared/TopInfoBanner.tsx
+++ src/components/shared/TopInfoBanner.tsx
@@ -1,27 +1,30 @@
 "use client";
-import React from 'react';
-import {useUrgentInfo} from "@/hooks/notifications/useUrgentInfo";
+import React from "react";
+import { useUrgentInfo } from "@/hooks/notifications/useUrgentInfo";
 
 const TopInfoBanner = () => {
+  const { data: info, isLoading, error } = useUrgentInfo();
 
-    const {data: info, isLoading, error} = useUrgentInfo()
-
-    if (isLoading) return <div
-        className="max-w-6xl fira-go page-wrapper py-2 text-center rounded-b-2xl bg-default-100 text-default-600">Loading...</div>
-    if (error) return <div>Error</div>
-
+  if (isLoading)
     return (
-      <>
-        <div
-          className="max-w-6xl text-white fira-go page-wrapper py-2 text-center rounded-b-2xl bg-default-100"
-          style={{ color: info && (info[0].color as string) }}
-        >
-          <div className="font-semibold">
-            {info && (info[0]?.content as string)}
-          </div>
-        </div>
-      </>
+      <div className="max-w-6xl fira-go page-wrapper py-2 text-center rounded-b-2xl bg-default-100 text-default-600">
+        Loading...
+      </div>
     );
+  if (error) return <div>Error</div>;
+
+  return (
+    <>
+      <div
+        className="max-w-6xl text-white fira-go page-wrapper py-2 text-center rounded-b-2xl bg-default-100"
+        style={{ color: info && (info[0]?.color as string) }}
+      >
+        <div className="font-semibold">
+          {info && (info[0]?.content as string)}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default TopInfoBanner;
