8975a56ce5bffb60a7b8c6267bae2c195be79027
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-27 21:11:44 +0100

    Refactor Home component to import SearchBox from hoppla

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,30 +1,31 @@
 import Hero from "@/components/partials/Hero";
 import OfferingsSection from "./(ride)/_components/OfferingsSection";
 import MobileAppBlock from "@/components/partials/MobileAppBlock";
-import {Suspense} from "react";
-import SearchBox from "@/components/partials/SearchBox";
+import { Suspense } from "react";
 import SvgMap from "@/components/shared/maps/SvgMap";
 import About from "@/components/shared/About";
+import { SearchBox } from "@/components/hoppla";
 
 const Home = async () => {
+  return (
+    <>
+      <Hero />
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className="page-wrapper py-12">
+          <SearchBox />
+        </div>
+      </Suspense>
+      <div className="bg-default-100 py-4 md:py-14 lg:py-28 fira-go">
+        <div className="page-wrapper mt-6 md:mt-0 mb-8 lg:mb-0">
+          <About />
+        </div>
+        <SvgMap />
+      </div>
 
-    return (
-        <>
-            <Hero/>
-            <Suspense fallback={<div>Loading...</div>}>
-                <div className="page-wrapper py-12"><SearchBox/></div>
-            </Suspense>
-            <div className="bg-default-100 py-4 md:py-14 lg:py-28 fira-go">
-                <div className="page-wrapper mt-6 md:mt-0 mb-8 lg:mb-0">
-                    <About/>
-                </div>
-                <SvgMap/>
-            </div>
-
-            <OfferingsSection/>
-            <MobileAppBlock/>
-        </>
-    );
-}
+      <OfferingsSection />
+      <MobileAppBlock />
+    </>
+  );
+};
 
 export default Home;
