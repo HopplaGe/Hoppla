edee41daf0c5f9615ed4051fb8beef08016f1e7b
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-03-27 21:12:23 +0100

    Refactor Providers component

--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,54 +1,41 @@
 "use client";
 import React from "react";
-import {NextUIProvider} from "@nextui-org/react";
-import {Analytics} from '@vercel/analytics/react';
-import {SpeedInsights} from "@vercel/speed-insights/next"
-import {NextIntlClientProvider} from "next-intl";
-import {SessionProvider} from "next-auth/react";
-import {
-    QueryClient,
-    QueryClientProvider,
-} from '@tanstack/react-query'
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import { NextUIProvider } from "@nextui-org/react";
+import { NextIntlClientProvider } from "next-intl";
+import { SessionProvider } from "next-auth/react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 type ProvidersProps = {
-    children: React.ReactNode;
-    locale: string;
-    messages: any;
+  children: React.ReactNode;
+  locale: string;
+  messages: any;
 };
 
-const Providers = ({children, locale, messages}: ProvidersProps) => {
+const Providers = ({ children, locale, messages }: ProvidersProps) => {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // staleTime: 6 * 1000,
+        // refetchInterval: 6 * 1000,
+      },
+    },
+  });
 
-    const queryClient = new QueryClient(
-        {
-            defaultOptions: {
-                queries: {
-                    // staleTime: 6 * 1000,
-                    // refetchInterval: 6 * 1000,
-                }
-            }
-        }
-    )
-
-    return (
-        <QueryClientProvider client={queryClient}>
-            {/*<ReactQueryDevtools/>*/}
-            <NextIntlClientProvider
-                onError={() => {
-                }}
-                locale={locale}
-                messages={messages}
-            >
-                <NextUIProvider>
-                    <SessionProvider>
-                        {children}
-                        <Analytics mode={'production'}/>
-                        <SpeedInsights/>
-                    </SessionProvider>
-                </NextUIProvider>
-            </NextIntlClientProvider>
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/*<ReactQueryDevtools/>*/}
+      <NextIntlClientProvider
+        onError={() => {}}
+        locale={locale}
+        messages={messages}
+      >
+        <NextUIProvider>
+          <SessionProvider>{children}</SessionProvider>
+        </NextUIProvider>
+      </NextIntlClientProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Providers;
